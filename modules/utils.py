11a3bab98779e9b5530a416996b72ba63d97356e
Vladimir Kirievskiy <vek2006_73@mail.ru>
2018-07-18 23:45:06 +0300

    docstrings

--- modules/utils.py
+++ modules/utils.py
@@ -36,18 +36,27 @@ def glue_result(categories, weights, is_sort):
 
 def round_vector(vector, digits_num):
     """
-    Round values of the column
+    Round values of the vector (column)
     @param vector: Column
-    @type vector: list
+    @type vector: list of floats
     @param digits_num: number of decimal places
     @type digits_num: int
-    @return:
-    @rtype:
+    @return: Vector with rounded values
+    @rtype: list
     """
     return [round(v, digits_num) for v in vector]
 
 
 def round_matrix(matrix, digits_num):
+    """
+    Round values of the square matrix
+    @param matrix: Column
+    @type matrix: list[][]
+    @param digits_num: number of decimal places
+    @type digits_num: int
+    @return: Matrix with rounded values
+    @rtype: list[][]
+    """
     result = matrix
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
@@ -56,6 +65,13 @@ def round_matrix(matrix, digits_num):
 
 
 def csv_to_list(file_path):
+    """
+    Import from csv-file to list
+    @param file_path: Path to the csv file (including its name)
+    @type file_path: string
+    @return: Imported values
+    @rtype: list
+    """
     with open(file_path, newline='', encoding='utf-8') as file_obj:
         reader = csv.reader(file_obj)
         result = list(reader)
@@ -63,6 +79,13 @@ def csv_to_list(file_path):
 
 
 def list_to_csv(file_path, list_to_write):
+    """
+    Export from list to csv file
+    @param file_path: Path to the csv file (including its name)
+    @type file_path: string
+    @param list_to_write: Values to export
+    @type list_to_write: list
+    """
     ensure_dir(file_path)
     with open(file_path, "w", newline='', encoding='utf-8') as file_obj:
         writer = csv.writer(file_obj, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
@@ -72,6 +95,13 @@ def list_to_csv(file_path, list_to_write):
 
 
 def str_list_to_float(processing_list):
+    """
+    Parser for list of strings which can include both float and '1/x'-like values
+    @param processing_list: List for parse
+    @type processing_list: list of strings
+    @return: Parsed list
+    @rtype: list of floats
+    """
     for i in range(len(processing_list)):
         for j in range(len(processing_list[0])):
             split_list = processing_list[i][j].split('/')
@@ -83,6 +113,15 @@ def str_list_to_float(processing_list):
 
 
 def get_fraction(float_value, round_digits_num):
+    """
+    Transform float value to '1/x'-like string. Integer values transformed to its common string representation
+    @param float_value: Value for transform
+    @type float_value: float
+    @param round_digits_num: Number of decimal places for round
+    @type round_digits_num: int
+    @return: Result of tranformation
+    @rtype: string
+    """
     float_to_fraction = {
         round(1 / 2, round_digits_num): '1/2',
         round(1 / 3, round_digits_num): '1/3',
@@ -94,7 +133,7 @@ def get_fraction(float_value, round_digits_num):
         round(1 / 9, round_digits_num): '1/9'
     }
     float_value = round(float_value, round_digits_num)
-    if (float_value > 0) and (float_value < 1):
+    if 0 < float_value < 1:
         return float_to_fraction[float_value]
     else:
         return str(int(float_value))
